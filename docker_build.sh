6c5cbb047cc0d268b09c5f5013c6366074e5b066
Andrey Kalugin <duyha.beitz@gmail.com>
2025-11-01 20:02:33 +0300

    Docker running with udp

--- docker_build.sh
+++ docker_build.sh
@@ -1,6 +1,14 @@
 # Build the image
-#sudo docker build --no-cache -t docker-server .
-sudo docker build -t docker-server .
+#sudo systemctl start docker
+
+sudo docker build --no-cache -t docker-server .
+sudo docker run -it -p 7777:7777/udp --name docker-server docker-server
+
+#sudo docker run -it --name docker-server --network host docker-server
+#sudo sudo docker run -it --name docker-server -p 7777:7777 docker-server
+
+
+#sudo docker build -t docker-server .
 
 # Stop and remove the old container if it exists
 #sudo docker stop game-server-name 2>/dev/null || true
